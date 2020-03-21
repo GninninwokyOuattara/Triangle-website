130eb4a7fb22dd5f46220e5ab588505b7851978f
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2020-03-21 20:29:58 +0000

    Can now receive form data

---
+++ src/utils/sub.js
@@ -0,0 +1,8 @@
+console.log("app started");
+
+// const form = document.querySelector("form");
+
+// form.addEventListener("submit", e => {
+//   e.preventDefault();
+//   console.log("button clicked");
+// });
